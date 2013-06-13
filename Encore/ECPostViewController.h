9c5a5864eb4b05f7190aa9f4de0dcf8a469ba676
Simon Bromberg <simon_2013@thenext36.ca>
2013-06-13 14:05:51 -0400

    added post view controller
    doesn't do anything yet

---
+++ Encore/ECPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECViewController.h
+//  Encore
+//
+//  Created by Shimmy on 2013-06-13.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECPostViewController : UIViewController
+
+@end
---
+++ Encore/ECPostViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECViewController.m
+//  Encore
+//
+//  Created by Shimmy on 2013-06-13.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import "ECPostViewController.h"
+
+@interface ECPostViewController ()
+
+@end
+
+@implementation ECPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
