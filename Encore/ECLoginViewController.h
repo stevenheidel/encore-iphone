26f471d52c5a3c550c19d2c96c5be71188aec2ce
Luis Ramirez <luis.ramireznossa@gmail.com>
2013-06-15 17:18:23 -0400

    addig pageing scrollview to login page

--- Encore/ECLoginViewController.h
+++ Encore/ECLoginViewController.h
@@ -9,7 +9,16 @@
 #import <UIKit/UIKit.h>
 #import <FacebookSDK/FacebookSDK.h>
 
-@interface ECLoginViewController : UIViewController<FBLoginViewDelegate>
+@interface ECLoginViewController : UIViewController<FBLoginViewDelegate, UIScrollViewDelegate>{
+    UIScrollView *descScrollView;
+    UIPageControl *pageControl;
+    NSArray *arrPages;
+}
 
+@property (nonatomic, retain) IBOutlet UIScrollView *descScrollView;
+@property (nonatomic, retain) IBOutlet UIPageControl *pageControl;
 @property (unsafe_unretained, nonatomic) IBOutlet FBLoginView *FBLoginView;
+
+- (IBAction)changePage;
+
 @end
--- Encore/ECLoginViewController.m
+++ Encore/ECLoginViewController.m
@@ -16,6 +16,9 @@
 
 @implementation ECLoginViewController
 
+@synthesize descScrollView;
+@synthesize pageControl;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -29,6 +32,59 @@
     [super viewWillAppear:animated];
     
     self.navigationController.navigationBarHidden = YES;
+
+    for (int i = 0; i < arrPages.count; i++) {
+        
+        NSDictionary *currPageItem = [arrPages objectAtIndex:i];
+        
+        //Create frame for each page
+        CGRect frame;
+        frame.origin.x = descScrollView.frame.size.width * i;
+        frame.origin.y = 0.0f;
+        frame.size = descScrollView.frame.size;
+        
+        //Create page and add components
+        UIView *subview = [[UIView alloc] initWithFrame:frame];
+        subview.backgroundColor = [UIColor blackColor];
+        
+        UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[currPageItem objectForKey:@"image"]]];
+        CGFloat imageX = subview.frame.size.width/2 - image.frame.size.width/2;
+        CGFloat imageY = subview.frame.size.height/2 - image.frame.size.height;
+        [image setFrame:CGRectMake(imageX, imageY, image.frame.size.width, image.frame.size.height)];
+        //image.center = subview.center;
+        [subview addSubview:image];
+        
+        UILabel *lblHeader = [[UILabel alloc] initWithFrame:CGRectMake(40.0f, (image.frame.origin.y + image.frame.size.height + 20.0f), 260.0f, 15.0f)];
+        lblHeader.backgroundColor = subview.backgroundColor;
+        lblHeader.textColor = [UIColor whiteColor];
+        lblHeader.text = [currPageItem objectForKey:@"header"];
+        lblHeader.textAlignment = NSTextAlignmentCenter;
+        [subview addSubview:lblHeader];
+        
+        UILabel *lblText = [[UILabel alloc] initWithFrame:CGRectMake(40.0f, (lblHeader.frame.origin.y + lblHeader.frame.size.height + 15.0f), 260.0f, 50.0f)];
+        lblText.backgroundColor = subview.backgroundColor;
+        lblText.textColor = [UIColor whiteColor];
+        
+        UIFont *font = lblText.font;
+        for(int i = 14; i > 10; i--)
+        {
+            // Set the new font size.
+            font = [lblText.font fontWithSize:i];            
+            CGSize constraintSize = CGSizeMake(260.0f, MAXFLOAT);
+            CGSize labelSize = [[currPageItem objectForKey:@"text"] sizeWithFont:font constrainedToSize:constraintSize lineBreakMode:NSLineBreakByWordWrapping];
+            if(labelSize.height <= 50.0f)
+                break;
+        }
+        lblText.text = [currPageItem objectForKey:@"text"];
+        lblText.textAlignment = NSTextAlignmentCenter;
+        lblText.numberOfLines = 0;
+        lblText.font = font;
+        //lblText.minimumScaleFactor = 0.1f;
+        lblText.adjustsFontSizeToFitWidth = YES;
+        [subview addSubview:lblText];
+        
+        [descScrollView addSubview:subview];
+    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -39,6 +95,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    NSString *myListPath = [[NSBundle mainBundle] pathForResource:@"LoginInfoPages" ofType:@"plist"];
+    arrPages = [[NSArray alloc]initWithContentsOfFile:myListPath];
+    NSLog(@"%@",arrPages);
+    
+    descScrollView.contentSize = CGSizeMake(descScrollView.frame.size.width * arrPages.count, descScrollView.frame.size.height);
 }
 
 - (void)viewDidUnload {
@@ -125,5 +187,22 @@
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
+#pragma mark - UIScrollView and UIPageControl
+
+- (void)scrollViewDidScroll:(UIScrollView *)sender {
+    // Update the page when more than 50% of the previous/next page is visible
+    CGFloat pageWidth = descScrollView.frame.size.width;
+    int page = floor((descScrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    self.pageControl.currentPage = page;
+}
+
+- (IBAction)changePage {
+    // update the scroll view to the appropriate page
+    CGRect frame;
+    frame.origin.x = self.descScrollView.frame.size.width * self.pageControl.currentPage;
+    frame.origin.y = 0;
+    frame.size = self.descScrollView.frame.size;
+    [self.descScrollView scrollRectToVisible:frame animated:YES];
+}
 
 @end
