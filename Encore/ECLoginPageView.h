108e0ae178e4e9dd26c01408c045eb3161fa272f
Luis Ramirez <luis.ramireznossa@gmail.com>
2013-06-15 18:45:21 -0400

    Added controller file for the custom view

---
+++ Encore/ECLoginPageView.h
@@ -0,0 +1,13 @@
+//
+//  ECLoginPageView.h
+//  Encore
+//
+//  Created by Luis Ramirez on 2013-06-15.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECLoginPageView : UIViewController
+
+@end
---
+++ Encore/ECLoginPageView.m
@@ -0,0 +1,38 @@
+//
+//  ECLoginPageView.m
+//  Encore
+//
+//  Created by Luis Ramirez on 2013-06-15.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import "ECLoginPageView.h"
+
+@interface ECLoginPageView ()
+
+@end
+
+@implementation ECLoginPageView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
