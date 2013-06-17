25a56944f29a540345b31992060c117d3cb2d792
Simon Bromberg <simon_2013@thenext36.ca>
2013-06-17 10:42:55 -0400

    added today vc

---
+++ Encore/ECTodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECTodayViewController.h
+//  Encore
+//
+//  Created by Shimmy on 2013-06-17.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTodayViewController : UIViewController
+
+@end
---
+++ Encore/ECTodayViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECTodayViewController.m
+//  Encore
+//
+//  Created by Shimmy on 2013-06-17.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import "ECTodayViewController.h"
+
+@interface ECTodayViewController ()
+
+@end
+
+@implementation ECTodayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
