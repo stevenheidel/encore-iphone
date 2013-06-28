7d569f86652b5edbfc13aef762e5d7d8f75c1e98
Simon Bromberg <simon_2013@thenext36.ca>
2013-06-28 16:42:12 -0400

    added file for picture picker

---
+++ Encore/ECPictureViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECPictureViewController.h
+//  Encore
+//  Display image selected by photo picker
+//  Created by Shimmy on 2013-06-28.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECPictureViewController : UIViewController
+
+@end
---
+++ Encore/ECPictureViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECPictureViewController.m
+//  Encore
+//
+//  Created by Shimmy on 2013-06-28.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import "ECPictureViewController.h"
+
+@interface ECPictureViewController ()
+
+@end
+
+@implementation ECPictureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
