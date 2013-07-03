365d36d21b56cc4bb1931362c12c62a4cafb4236
Luis Ramirez <luis.ramireznossa@gmail.com>
2013-07-03 16:13:02 -0400

    new future concert detail file added

---
+++ Encore/ECFutureConcertViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECFutureConcertViewController.h
+//  Encore
+//
+//  Created by Luis Ramirez on 2013-07-03.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECFutureConcertViewController : UIViewController
+
+@end
---
+++ Encore/ECFutureConcertViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECFutureConcertViewController.m
+//  Encore
+//
+//  Created by Luis Ramirez on 2013-07-03.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#import "ECFutureConcertViewController.h"
+
+@interface ECFutureConcertViewController ()
+
+@end
+
+@implementation ECFutureConcertViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
