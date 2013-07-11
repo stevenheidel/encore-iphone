29941ac49569a6638d1c9c85314205084f032b33
Simon Bromberg <simon_2013@thenext36.ca>
2013-07-11 01:59:57 -0400

    Alert tags,started consolidating into one file
    Maybe a good idea, not sure yet

---
+++ Encore/ECAlertTags.h
@@ -0,0 +1,19 @@
+//
+//  ECAlertTags.h
+//  
+//
+//  Created by Shimmy on 2013-07-11.
+//
+//
+
+#ifndef _ECAlertTags_h
+#define _ECAlertTags_h
+
+typedef enum {
+    ECNotLoggedInAlert,
+    PhotoSourcePickerTag,
+    AddConcertConfirmTag,
+    RemoveConcertConfirmTag
+}ECAlertTags;
+
+#endif
