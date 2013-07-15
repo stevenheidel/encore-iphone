3816c4ef312096e4116f7bcdc2b8457a5cb08fff
Simon Bromberg <simon_2013@thenext36.ca>
2013-07-15 03:34:46 -0400

    TRYING OUT agmedallion for nice circular profile image. will probably get rid of it

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 3
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
+// AGMedallionView
+#define COCOAPODS_POD_AVAILABLE_AGMedallionView
+#define COCOAPODS_VERSION_MAJOR_AGMedallionView 1
+#define COCOAPODS_VERSION_MINOR_AGMedallionView 1
+#define COCOAPODS_VERSION_PATCH_AGMedallionView 0
+
 // Facebook-iOS-SDK
 #define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
 #define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
