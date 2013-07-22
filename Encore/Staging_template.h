1349b4206c310ac8d0f0dc58da40c6b80cbfc38f
Simon Bromberg <simon_2013@thenext36.ca>
2013-07-22 10:23:28 -0400

    added staging template

---
+++ Encore/Staging_template.h
@@ -0,0 +1,18 @@
+//
+//  Staging.h
+//  Encore
+//
+//  Created by Shimmy on 2013-06-26.
+//  Copyright (c) 2013 Encore. All rights reserved.
+//
+
+#ifndef Encore_Staging_h
+#define Encore_Staging_h
+
+#define STAGING 1
+
+#endif
+
+#define RETURN_TEST_DATA 0
+#define FLURRY_LOGGING 0
+#define IN_BETA 1
