6c978ed8eb01c03a305469641a3580099c55c28a
Simon Bromberg <simon_2013@thenext36.ca>
2013-07-22 12:58:49 -0400

    added comment

--- Encore/NSDate+JSON.m
+++ Encore/NSDate+JSON.m
@@ -4,13 +4,13 @@
 //
 //  Created by Shimmy on 2013-06-13.
 //  Copyright (c) 2013 Encore. All rights reserved.
-//
+//  Convert an NSDate into a json friendly string
 
 #import "NSDate+JSON.h"
 @implementation NSDate (JSON)
 
 -(NSString*) jsonString {
-    return [self description];
+    return [self description]; //tried various things, but this ended up working fine. Probably should change
 }
 
 @end
