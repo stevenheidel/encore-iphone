afb23fa5f89372bd40562cdddbf51cc5c536d855
Simon Bromberg <simon_2013@thenext36.ca>
2013-08-08 13:09:49 -0400

    Fixes #91

--- Encore/ECGridViewController.m
+++ Encore/ECGridViewController.m
@@ -15,6 +15,10 @@
 #import "UIColor+EncoreUI.h"
 #import "UIFont+Encore.h"
 #import "ECPostViewController.h"
+#import "ECAppDelegate.h"
+#import "ECAlertTags.h"
+#import "ATAppRatingFlow.h"
+#import "EncoreURL.h"
 
 @implementation ECPostCell
 
@@ -60,12 +64,20 @@
     self.navigationItem.titleView = encoreLogo;
     self.collectionView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
     UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    UIImage *leftButImage = [UIImage imageNamed:@"backButton.png"]; //stretchableImageWithLeftCapWidth:10 topCapHeight:10];
+    UIImage *leftButImage = [UIImage imageNamed:@"backButton"]; //stretchableImageWithLeftCapWidth:10 topCapHeight:10];
     [leftButton setBackgroundImage:leftButImage forState:UIControlStateNormal];
     [leftButton addTarget:self action:@selector(backButtonWasPressed) forControlEvents:UIControlEventTouchUpInside];
     leftButton.frame = CGRectMake(0, 0, leftButImage.size.width, leftButImage.size.height);
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithCustomView:leftButton];
     self.navigationItem.leftBarButtonItem = backButton;
+    
+    UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage *rightButImage = [UIImage imageNamed:@"shareButton"]; //stretchableImageWithLeftCapWidth:10 topCapHeight:10];
+    [rightButton setBackgroundImage:rightButImage forState:UIControlStateNormal];
+    [rightButton addTarget:self action:@selector(shareTapped) forControlEvents:UIControlEventTouchUpInside];
+    rightButton.frame = CGRectMake(0, 0, rightButImage.size.width, rightButImage.size.height);
+    UIBarButtonItem* shareButton = [[UIBarButtonItem alloc] initWithCustomView:rightButton];
+    self.navigationItem.rightBarButtonItem = shareButton;
 }
 -(void) backButtonWasPressed {
     [self.navigationController popViewControllerAnimated:YES];
@@ -146,5 +158,107 @@
     
     return [NSDictionary dictionaryWithObjectsAndKeys:[self.posts objectAtIndex:newIndex], @"dic", [NSNumber numberWithInt:newIndex], @"index",nil];
 }
+
+#pragma mark FB Sharing
+-(void) shareTapped {
+    if([ApplicationDelegate isLoggedIn]) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:ECLoginCompletedNotification object:nil];
+        [[ATAppRatingFlow sharedRatingFlow] logSignificantEvent];
+        [Flurry logEvent:@"Share_Tapped_Concert"];
+        [self share];
+    }
+    else {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Login", nil) message:NSLocalizedString(@"To share this concert, you must first login", nil) delegate:self cancelButtonTitle:NSLocalizedString(@"Cancel", nil) otherButtonTitles:NSLocalizedString(@"Login", nil), nil];
+        alert.tag = ECShareNotLoggedInAlert;
+        [alert show];
+    }
+}
+
+-(void) share {
+    [self shareWithTaggedFriends:nil];
+}
+
+-(void) shareWithTaggedFriends: (NSArray*) taggedFriends {
+    NSLog(@"Sharing with Facebook from Concert detail view controller");
+    NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:ShareConcertURL,self.concert.eventID]];
+    
+    FBShareDialogParams* params = [[FBShareDialogParams alloc] init];
+    params.link = url;
+    if(taggedFriends)
+        [params setFriends:[taggedFriends valueForKey:@"id"]];
+    
+    //    params.description =  [NSString stringWithFormat:@"Check out photos and videos from %@'s %@ show in %@ at the %@ on Encore.",[self.concert artistName], [self.concert niceDate], [self.concert city], [self.concert venueName]];
+    if ([FBDialogs canPresentShareDialogWithParams:params]) {
+        [FBDialogs presentShareDialogWithParams:params clientState:nil handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
+            if(error) {
+                NSLog(@"Error sharing concert: %@", error.description);
+                [Flurry logEvent:@"Concert_Share_To_FB_Fail" withParameters:[NSDictionary dictionaryWithObjectsAndKeys:url.absoluteString, @"url", error.description, @"error", nil]];
+            } else {
+                NSLog(@"Success sharing concert!");
+                [Flurry logEvent:@"Concert_Share_To_FB_Success" withParameters:[NSDictionary dictionaryWithObjectsAndKeys:url.absoluteString, @"url", nil]];
+            }
+            
+        }];
+    }
+    else {
+        NSMutableDictionary *params2 =
+        [NSMutableDictionary dictionaryWithObjectsAndKeys:
+         [NSString stringWithFormat:@"%@ on Encore",[self.concert eventName]], @"name",
+         [NSString stringWithFormat:@"Check out photos and videos from %@'s %@ show on Encore.",[self.concert eventName], [self.concert niceDate]], @"caption",
+         @"Encore is a free iPhone concert app that collects photos and videos from live shows and helps you keep track of upcoming shows in your area.",@"description",
+         [NSString stringWithFormat:ShareConcertURL,self.concert.eventID], @"link",
+         [NSString stringWithFormat:@"%@",[self.concert imageURL].absoluteString], @"picture",
+         nil];
+        [FBWebDialogs presentFeedDialogModallyWithSession:[FBSession activeSession]
+                                               parameters:params2
+                                                  handler:
+         ^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
+             if (error) {
+                 // Error launching the dialog or publishing a story.
+                 NSLog(@"Error publishing story.");
+             } else {
+                 if (result == FBWebDialogResultDialogNotCompleted) {
+                     // User clicked the "x" icon
+                     NSLog(@"User canceled story publishing.");
+                 } else {
+                     // Handle the publish feed callback
+                     NSDictionary *urlParams = [self parseURLParams:[resultURL query]];
+                     if (![urlParams valueForKey:@"post_id"]) {
+                         // User clicked the Cancel button
+                         NSLog(@"User canceled story publishing.");
+                     } else {
+                         // User clicked the Share button
+                         NSString *msg = @"Posted to facebook";
+                         NSLog(@"%@", msg);
+                         [Flurry logEvent:@"Successfully_Posted_To_Facebook_With_Feed_Dialog" withParameters:[NSDictionary dictionaryWithObjectsAndKeys:@"Concert",@"type", nil]];
+                         // Show the result in an alert
+                         [[[UIAlertView alloc] initWithTitle:@"Result"  //TODO: replace with HUD
+                                                     message:msg
+                                                    delegate:nil
+                                           cancelButtonTitle:@"OK!"
+                                           otherButtonTitles:nil]
+                          show];
+                     }
+                 }
+             }
+         }];
+    }
+}
+
+/**
+ * A function for parsing URL parameters.
+ */
+- (NSDictionary*)parseURLParams:(NSString *)query {
+    NSArray *pairs = [query componentsSeparatedByString:@"&"];
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    for (NSString *pair in pairs) {
+        NSArray *kv = [pair componentsSeparatedByString:@"="];
+        NSString *val =
+        [kv[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        params[kv[0]] = val;
+    }
+    return params;
+}
+
 
 @end
