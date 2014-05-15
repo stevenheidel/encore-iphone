6c4aa6ec83caa47ad410b5cbf21ac8736f301db9
simonbromberg <simon@encoretheapp.com>
2014-05-15 15:42:19 -0400

    removed testflight sdk

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -48,12 +48,6 @@
 #define COCOAPODS_VERSION_MINOR_Reachability 1
 #define COCOAPODS_VERSION_PATCH_Reachability 1
 
-// TestFlightSDK
-#define COCOAPODS_POD_AVAILABLE_TestFlightSDK
-#define COCOAPODS_VERSION_MAJOR_TestFlightSDK 3
-#define COCOAPODS_VERSION_MINOR_TestFlightSDK 0
-#define COCOAPODS_VERSION_PATCH_TestFlightSDK 0
-
 // XCDYouTubeVideoPlayerViewController
 #define COCOAPODS_POD_AVAILABLE_XCDYouTubeVideoPlayerViewController
 #define COCOAPODS_VERSION_MAJOR_XCDYouTubeVideoPlayerViewController 1
